f32e6d5ddabb13f2d367947fd006cb7c6d9dcd05
kashif <kashif.tufail17@gmail.com>
2025-06-25 15:58:37 +0500

    add multitag auto complete in questions

--- questions/forms.py
+++ questions/forms.py
@@ -1,11 +1,23 @@
+# questions/forms.py
 from django import forms
 from .models import Question
-from taggit.forms import TagWidget
+from django_select2.forms import Select2TagWidget   # the only widget we need
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['title', 'detail', 'tags']
+        fields = ["title", "detail", "tags"]
         widgets = {
-            'tags': TagWidget(attrs={'placeholder': 'Add tags'}),
+            "title":  forms.TextInput(attrs={"class": "w-full p-2 border rounded"}),
+            "detail": forms.Textarea(attrs={"class": "w-full p-2 border rounded"}),
+            # Select2 sends Ajax to our autocomplete URL and lets users add new tags
+            "tags": Select2TagWidget(
+                attrs={
+                    "class": "w-full",
+                    "data-tags": "true",                # allow new tags
+                    "data-ajax--url": "/autocomplete/tags/",
+                    "data-placeholder": "Add tags…",
+                    "data-minimum-input-length": "1",
+                }
+            ),
         }
